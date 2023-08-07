9d708a3455105900ae7d17042db4e19e7aec50a0
v <zh1024@zohomail.cn>
2023-08-07 20:50:02 +0800

    update

--- init.sh
+++ init.sh
@@ -2,10 +2,9 @@
 
 apt update
 apt upgrade -y
-
-apt install -y openssh
-echo "请输入新ssh密码"
-passwd
+pkg install termux-api
+termux-setup-storage
+echo "请在应用权限管理中手动给予termux存储权限"
 
 ./zsh.sh
 ./glibc.sh
@@ -14,7 +13,8 @@ passwd
 # apt install -y sqlite
 # apt install -y openjdk-17
 
+apt install -y openssh
+echo "请输入新ssh密码"
+passwd
 echo "可以使用sshh命令开启ssh服务并显示连接命令"
 echo "使用命令\"pkill sshd\"结束ssh服务"
-
-echo "请在应用权限管理中手动给予termux存储权限"
