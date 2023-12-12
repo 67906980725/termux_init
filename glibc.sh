f7bf01c055194ba5cd7c9502fe24c6e1fac0da89
ㅤ <zh1024@zohomail.cn>
2023-12-12 22:29:07 +0800

    Update glibc.sh

--- glibc.sh
+++ glibc.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-
+# 安装glibc
 source ./cp_conf.sh
 
 pkg ins pacman -y
-arch='aarch64' # aarch64 或 armv7
-wget ${GITHUB_PROXY}https://github.com/Maxython/glibc-for-termux/releases/download/20221025/gpft-20221025-${arch}.tar.xz
+github_proxy='https://download.nuaa.cf/'
+arch='aarch64'
+wget ${github_proxy}https://github.com/Maxython/glibc-for-termux/releases/download/20221025/gpft-20221025-${arch}.tar.xz
 tar xJf gpft-20221025-${arch}.tar.xz
 pacman -U glibc-for-termux/*
+#grun --shell # 起一个可以访问 glibc 命令的shell
