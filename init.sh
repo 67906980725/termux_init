fde21ac7863f6d68156d8ddaaf0c1e821e362373
ㅤ <zh1024@zohomail.cn>
2023-12-16 11:03:32 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,13 @@ apt upgrade -y
 # https://wiki.termux.com/wiki/Termux-services
 # https://blog.csdn.net/YiBYiH/article/details/127294017 
 # $PREFIX/var/service/<your_service_name> 
+
+# 要同时安装和 termux 同一证书(同一渠道下)的 termux-api.apk 并授予所有权限重启手机后才能正常使用 api
+pkg install -y termux-api
+
 # 创建 service 后如果启动服务报错就重启 termux, 启动服务后如果没生效就重启设备
-pkg install -y termux-api termux-services
+pkg install -y termux-services
+
 termux-setup-storage
 echo "请在应用权限管理中手动给予termux存储权限"
 
